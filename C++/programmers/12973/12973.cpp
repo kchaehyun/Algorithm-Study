fcfaf7dd4edaef1ffb55f9220bf8852ea259b25a
kchaehyun <rlacogus1221@naver.com>
2026-08-18 23:52:47 +0900

    add programmers 12973

---
+++ C++/programmers/12973/12973.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    for(char c : s) {
+        if(!st.empty() && st.top() == c)
+            st.pop();
+        else
+            st.push(c);
+    }
+    if(!st.empty()) return 0;
+    return 1;
+}
